c5bcd41fbdfa48c794357dc9c676a92ef39c2f76
eunicollas <80913209+eunicollas@users.noreply.github.com>
2021-05-28 22:50:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let numero1 = prompt('Insira o primeiro numero');
+let numero2 = prompt('Insira o segundo numero');
+
+function calcularProduto(a, b) {
+    return a * b;
+}  
+const x = calcularProduto(numero1, numero2);
+
+alert(`O resultado é ${x}`)
